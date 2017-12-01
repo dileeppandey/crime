59c522b2949ef9baa2b850ff4f38cf0ec2649214
Varun <varun1309@bitbucket.org>
2017-11-30 17:42:05 -0700

    sample sparql queries exec

--- app/crime.js
+++ app/crime.js
@@ -7,10 +7,25 @@ var util = require('util');
 var endpoint = 'http://dbpedia.org/sparql';
 
 router.get('/fetch/:city', function(req, res, next){
-	// var body = req.body;
+    // var body = req.body;
+    var city = req.params.city;
     console.log('got city name'+req.params.city);
-    res.json('hahahah');
 
+    // Get the leaderName(s) of the given citys
+    // if you do not bind any city, it returns 10 random leaderNames
+    var query = "SELECT * FROM <http://dbpedia.org> WHERE {?city <http://dbpedia.org/property/leaderName> ?leaderName} LIMIT 10";
+    var client = new SparqlClient(endpoint);
+    console.log("Query to " + endpoint);
+    console.log("Query: " + query);
+    client.query(query)
+    //   .bind('city', 'db:Chicago')
+      //.bind('city', 'db:Tokyo')
+    //   .bind('city', 'db:Casablanca')
+    //   .bind('city', '<http://dbpedia.org/resource/Vienna>')
+      .execute(function(error, results) {
+    //   process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
+      res.json(results);
+    });
 });
 
 router.get('/', function(req, res, next){
