c3a5dc57fa96696c80547c9c6297c2a781623bf1
Dileep Pandey <pandey.dileep.in@gmail.com>
2017-12-04 01:32:59 -0700

    UI updated, change api url and test

--- crime-ui/app/components/search/search.component.js
+++ crime-ui/app/components/search/search.component.js
@@ -22,8 +22,15 @@ angular.module('search').component('search', {
 
 angular.module('search').controller('TypeaheadCtrl', ['$scope', '$http', function ($scope, $http) {
     var states = { "AL": "Alabama", "AK": "Alaska", "AS": "American Samoa", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "District Of Columbia", "FM": "Federated States Of Micronesia", "FL": "Florida", "GA": "Georgia", "GU": "Guam", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MH": "Marshall Islands", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", "MP": "Northern Mariana Islands", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PW": "Palau", "PA": "Pennsylvania", "PR": "Puerto Rico", "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VI": "Virgin Islands", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming" }
+    $scope.location = null;
+    $scope.crimeData = {};
+    $scope.fedExpense = {};
+    $scope.demography = {};
     
-
+    $scope.isLocAvailable = false;
+    $scope.isCrimeDataAvailable = false;
+    $scope.isFedExpenseAvailable = false;
+    $scope.isDemographyAvailable = false;
 
     // Any function returning a promise object can be used to load values asynchronously
     $scope.getLocation = function (val) {
@@ -44,35 +51,49 @@ angular.module('search').controller('TypeaheadCtrl', ['$scope', '$http', functio
         console.log('inside getCityData');
         console.log(params);
         var data = params.split(',');
-        var host = 'http://10.152.181.9:3000/';
+        
+        var host = 'http://localhost:3000/';
+
+        $scope.location = params;
 
         //http://10.152.181.9:3000/federalExpenses/ArizoNA
         //http://10.152.181.9:3000/crimeData/tempe/arizona
         //http://10.152.181.9:3000/educationData/arizona
 
-        var fedExpenseUrl = host + 'federalExpenses/';
-        var crimeDataUrl = host + 'crimeData/';
-        var demoUrl = host + 'educationData/';
+        var fedExpenseUrl = host + 'federalExpenses/'.toLocaleLowerCase();
+        var crimeDataUrl = host + 'crimeData/'.toLocaleLowerCase();
+        var demoUrl = host + 'educationData/'.toLocaleLowerCase();
 
         if(data[0] !== undefined && data[1] !== undefined) {
-            var city = data[0].trim();
-            var state = states[data[1].trim()].trim();
-            crimeDataUrl = host + 'crimeData/' + city + '/' + state;
-            fedExpenseUrl = host + 'federalExpenses/' + state;
-            demoUrl = host + 'educationData/' + state;
+            
+            $scope.isLocAvailable = true;            
+            var city = data[0].trim().toLocaleLowerCase();
+            var state = states[data[1].trim()].trim().toLocaleLowerCase();
+            crimeDataUrl = crimeDataUrl + city + '/' + state;
+            fedExpenseUrl = fedExpenseUrl + state;
+            demoUrl = demoUrl + state;
         }
 
-        $scope.fedExpense = $http.get(fedExpenseUrl).then(function (response) {
-            console.log('fed', response);
-            return response;
+        console.log('fedExpenseUrl',fedExpenseUrl);
+        console.log('crimeDataUrl', crimeDataUrl);
+        console.log('demoUrl', demoUrl);
+        
+        $http.get(crimeDataUrl).then(function (response) {
+            console.log('response.data.crimeData', response.data.crimeData);
+            $scope.crimeData = response.data.crimeData;
+            $scope.isCrimeDataAvailable = true;
         });
-        $scope.demography = $http.get(demoUrl).then(function (response) {
-            console.log('demo', response);
-            return response;
+
+        $http.get(demoUrl).then(function (response) {
+            console.log('demo', response.data);
+            $scope.isFedExpenseAvailable = true;
+            $scope.demography = response.data.educationData;
         });
-        $scope.crimeData = $http.get(crimeDataUrl).then(function (response) {
-            console.log('crime', response);
-            return response;
+
+        $http.get(fedExpenseUrl).then(function (response) {
+            console.log('fed', response.data);
+            $scope.isDemographyAvailable = true;
+            $scope.fedExpense = response.data;
         });
     }
 }]);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 3000;
 var path = require('path');
 
 app.use(express.static(__dirname + '/views'));
-var crime = require('./app/crime');
+// var crime = require('./app/crime');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,6 +12,53 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use('/', crime);
+var fedExData = {
+    "state": "Arizona",
+    "federalExpenses": {
+        "AmericanIndianEducation": "10,235 ",
+        "AssistanceInFederallyAffectedAreas": "149,281 ",
+        "BlockGrantForSchoolImprovement": "75,805 ",
+        "EnglishLanguageAcquisition": "14,128 ",
+        "GrantsForDisadvantaged": "342,030 ",
+        "RehabilitationServices": "65,364 ",
+        "SpecialEducation": "203,193 ",
+        "StudentFinancialAssistance": "1,312,501 "
+    }
+};
+var crimeData = {
+    "crimeData": {
+        "population": "164,008",
+        "Aggravated_Assault": "500",
+        "Arson": "28",
+        "Burglary": "1,579",
+        "Larceny_Theft": "6,804",
+        "Motor_vehicle_theft": "550",
+        "Murder": "5",
+        "Rape": "45",
+        "Robbery": "237"
+    }
+};
+var demoData = {
+    "educationData": {
+        "HighSchoolDiplomaOnlyPercentage": "24.4",
+        "BachelorDegreeOrHigherPercentage": "27.5",
+        "LessThanHighSchoolPercentage": "14"
+    }
+};
+
+app.get('/crimedata/tempe/arizona', function(req, res) {
+    res.send(crimeData);
+});
+
+app.get('/educationdata/arizona', function (req, res) {
+    res.send(demoData);
+});
+
+app.get('/federalexpenses/arizona', function (req, res) {
+    res.send(fedExData);
+});
+
+
+// app.use('/', crime);
 
 app.listen(3000);
